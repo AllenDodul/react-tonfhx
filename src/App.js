a7a5345ef12745ab241515e474140ec4225bd651
AllenDodul <109014717+AllenDodul@users.noreply.github.com>
2022-11-26 19:25:54 +0600

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,47 +2,58 @@ import React from "react";
 import {useState} from 'react';
 import "./style.css";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBHbuG3-XTLTwVlMBHF0_cdgmLWYMIC7e0",
-  authDomain: "contactform2-f9330.firebaseapp.com",
-  databaseURL: "https://contactform2-f9330-default-rtdb.firebaseio.com",
-  projectId: "contactform2-f9330",
-  storageBucket: "contactform2-f9330.appspot.com",
-  messagingSenderId: "404463780033",
-  appId: "1:404463780033:web:fe3d3f80b17499f653d440"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
 export default function App() {
   const [userInfo , setUserInfo] = useState({});
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [number, setNumber] = useState();
 
+  const handleName = (e) =>{
+    setName(e.target.value);
+  }
+
+  const handleEmail = (e) =>{
+    setEmail(e.target.value);
+  }
+
+  const handleNumber = (e) =>{
+    setNumber(e.target.value);
+  }
+
+  const assignUser = () =>{
+    setUserInfo({
+      name: name,
+      email: email,
+      age: number,
+    });
+    console.log(userInfo);
+  }
+  
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <input 
+        onChange={handleName}
         type="text"
         className="name"
         placeholder="Name"
+        value={name}
       />
       <input 
         type="email"
         className="email"
         placeholder="Email"
+        onChange={handleEmail}
+        value={email}
       />
       <input 
         type="number"
         className="age"
         placeholder="Age"
+        onChange={handleNumber}
+        value={number}
       />
-      <button>Submit</button>
+      <button onClick={assignUser}>Submit</button>
     </div>
   );
 }
