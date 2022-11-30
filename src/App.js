c15f0292b5ef86a2cbdbab4174e96f14019a7bb4
AllenDodul <109014717+AllenDodul@users.noreply.github.com>
2022-11-30 20:26:39 +0600

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,6 +3,30 @@ import {useState} from 'react';
 import "./style.css";
 
 const App = () =>{
+  const [name ,setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [roll, setRoll] = useState('');
+  const [clas, setClas] = useState('');
+
+  const handleName = (e) =>{
+    setName(e.target.value);
+  }
+
+  const handleEmail = (e) =>{
+    setEmail(e.target.value);
+  }
+  
+  const handleRoll = (e) =>{
+    setRoll(e.target.value)
+  }
+
+  const handleClas = (e) =>{
+    setClas(e.target.value);
+  }
+
+  const showData =() =>{
+    
+  }
   return(
     <div className="from-container">
     <div id="form">
@@ -10,23 +34,37 @@ const App = () =>{
         type="text" 
         className="name" 
         placeholder="Enter Your name"
+        onChange={handleName}
+        value={name}
       />
       <input 
         type="email" 
         className="email" 
         placeholder="Enter Your Email"
+        onChange={handleEmail}
+        value={email}
       />
       <input 
         type="text"
         className="roll"
         placeholder="Enter your Roll number"
+        onChange={handleRoll}
+        value={roll}
       />
       <input 
         type="text"
         className="class"
         placeholder="Enter your class"
+        onChange={handleClas}
+        value={clas}
       />
-      <button type="submit" className="button">Submit</button>
+      <button 
+        type="submit" 
+        className="button"
+        onClick={showData}
+      >
+        Submit
+      </button>
     </div>
   </div>
   )
